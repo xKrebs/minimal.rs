21d8fbf4e0619789ac6cde38d8985035b82124ae
Krebs <cristian_ricci@outlook.it>
2023-04-08 11:51:08 +0200

    Add project in github

--- src/main.rs
+++
@@ -1,6 +0,0 @@
-extern crate minimal;
-use minimal::{ MinimalDocument, MinimalElement, MinimalHtml, MinimalList, MinimalWindow};
-fn main(){
-    let window = minimal::window();
-    let document = minimal::document();
-}
